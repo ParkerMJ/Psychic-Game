f047069f606d0b26dd14c647a859ce05847b705f
ParkerMJ <parkermattjohnson@gmail.com>
2019-02-18 21:46:47 -0800

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var wins=0;
+var losses=0;
+var guessesLeft=0;
+var guesses=0;
+var computerGuess=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guess="";
+
+
+    var computer=computerGuess[Math.floor(Math.random()*computerGuess.length)];
+    console.log(computer);

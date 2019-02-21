331b2fa2cbdcfbf980a6fb329111166f802aff20
ParkerMJ <parkermattjohnson@gmail.com>
2019-02-20 19:11:54 -0800

    v.1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,45 @@
 var wins=0;
 var losses=0;
-var guessesLeft=0;
+var guessesLeft=9;
 var guesses=0;
-var computerGuess=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var guess="";
+var computerGuess=0;
+var randomLetter=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   
+function stats(){
+    document.getElementById("wins").innerHTML="Wins: "+wins;
+    document.getElementById("losses").innerHTML="Losses: "+losses;
+    document.getElementById("guessesLeft").innerHTML="Guesses Left: "+guessesLeft;
+    document.getElementById("guesses").innerHTML="Your Guesses So Far: "+guesses;
+}
 
+function randomizer(){
+    computerGuess=randomLetter[Math.floor(Math.random()*randomLetter.length)];
+    console.log("Computer Guess: "+computerGuess);
+}
 
-    var computer=computerGuess[Math.floor(Math.random()*computerGuess.length)];
-    console.log(computer);
+stats();
+randomizer();
+
+document.onkeyup=function(event){
+    var letterGuess=event.key.toLowerCase();
+    
+    if(letterGuess!==computerGuess){
+        guesses++;
+        guessesLeft--;
+        stats();
+    }
+    if(letterGuess===computerGuess){
+        wins++;
+        guesses=0;
+        guessesLeft=9;
+        stats();
+        randomizer();
+    }
+    if(guessesLeft===0){
+        losses++;
+        guesses=0;
+        guessesLeft=9;
+        stats();
+        randomizer();
+    }
+}
